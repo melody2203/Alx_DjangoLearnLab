e800f02f9fcd9c2c5024e59ea2af41a0d762dc78
Merertu Philipose <merertuphilip@gmail.com>
2025-09-21 19:27:24 +0300

    final update

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,11 +9,12 @@ def get_books_by_author(author_name):
 
 # 2. List all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)  # ✅ this is the missing line
+    library = Library.objects.get(name=library_name)
     return library.books.all()
 
 # 3. Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)   # ✅ now using Librarian.objects.get
+
 
