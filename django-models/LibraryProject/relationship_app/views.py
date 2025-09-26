3a631299d86e790d437af7b733418455b71574e2
Merertu Philipose <merertuphilip@gmail.com>
2025-09-26 18:00:02 +0300

    Add role-based

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.generic.detail import DetailView   # ✅ correct import
-from .models import Library, Book   # ✅ explicit import
+from django.views.generic.detail import DetailView   
+from .models import Library, Book   
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 # User Registration View
-def register_view(request):
+def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
