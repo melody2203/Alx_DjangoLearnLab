c4df235ddac072b50cd7700ef4bd74fa1c7941c2
Merertu Philipose <merertuphilip@gmail.com>
2025-10-01 02:25:16 +0300

    Add custom model

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
