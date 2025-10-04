22b6283837d12bf159d40f16153da8d9113138d4
Merertu Philipose <merertuphilip@gmail.com>
2025-10-04 13:39:59 +0300

    add serializer and api view

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'author']
