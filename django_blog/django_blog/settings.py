e882ef3ff17149c00eb1f94e309c4678221a58d9
Merertu Philipose <merertuphilip@gmail.com>
2025-10-13 13:15:55 +0300

    update setting of django blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,9 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',           # Added USER (empty for SQLite)
+        'PASSWORD': '',       # Added PASSWORD (empty for SQLite)
+        'HOST': '',           # Added HOST (empty for SQLite)
+        'PORT': '',           # Added PORT (empty for SQLite)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
